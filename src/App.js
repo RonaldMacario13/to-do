7c351b9d45953e8b7bb3a6e01ae1ed1eeae2e091
Ronald Macario <ronaldmacario13@gmail.com>
2023-03-14 09:44:18 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            newTask: '',
+            tasks:  []
+        };
+    }
+    handleAddTask = () => {
+        if(this.state.newTask === '') return;
+        const newTasks = [...this.state.tasks, this.state.newTask]
+        this.setState({
+            newTask: '',
+            tasks: newTasks
+        });
+    }
+    handleDeleteTask = (index) => {
+        const newTasks = [...this.state.tasks]
+        newTasks.splice(index, 1);
+        this.setState({tasks: newTasks})
+    }
+    render() {
+        return (
+            <div>
+                <input type = "text" placeholder="Nova tarefa" value={this.state.newTask} onChange={(event) => this.setState ({newTask: event.target.value})}/>
+                <button onClick={this.handleAddTask}>Adicionar</button>
+                <ul>
+                    {this.state.tasks.map((task, index) => (<li key = {index}>{task} <button onClick={() => this.handleDeleteTask(index)}>Excluir</button></li>))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
